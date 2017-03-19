bc0d7d27aacfd99424ff150ff41eb77580510d10
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-19 13:28:28 -0700

    fix typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user.authenticate(params[:password])
-      render json: { authToken: @user.auth_token, client: @user.is_a? Client }
+      render json: { authToken: @user.auth_token, client: (@user.is_a? Client) }, status: :created
     else
       render json: {}, status: :unauthorized
     end
