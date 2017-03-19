82e2b3f076811e24f3d183083b58199769933766
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-18 18:14:00 -0700

    create with errors n such

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,16 @@
 class ClientsController < ApplicationController
   def create
-    @client = Client.new()
+    @client = Client.new(client_params)
+    if @client.save
+      status 201
+    else
+      render json: { errors: @client.errors.full_messages }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:first_name, :last_name, :email, :street_address, :city, :state, :zipcode, :phone_number, :password)
   end
 end
