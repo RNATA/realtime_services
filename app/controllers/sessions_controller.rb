33b67c27d123b4584eaa22449dd0483c5a2878e3
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-18 17:43:56 -0700

    passwords?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
   def create
     p params
+    @client = Client.find_by(email: params[:email])
+    if @client.authenticate(params[:password])
+      render json: { token: @client.id }
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 end
