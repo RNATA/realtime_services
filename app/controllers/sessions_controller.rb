bdc25c350997dfab0a924ac4ab76c76b5dc05b6d
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-19 12:24:12 -0700

    fix auth issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user.authenticate(params[:password])
-      render json: { authToken: @user.set_auth_token }
+      render json: { authToken: @user.auth_token }
     else
       render json: {}, status: :unauthorized
     end
