753c8e6fd69c440388f520b0d9240f25030bbd2f
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-19 14:20:38 -0700

    auth

--- app/controllers/provider_services_controller.rb
+++ app/controllers/provider_services_controller.rb
@@ -1,6 +1,7 @@
 class ProviderServicesController < ApplicationController
   def create
-    @provider = Provider.find_by(auth_token: request.headers["auth_token"])
+    p request.headers
+    @provider = Provider.find_by(auth_token: request.headers["auth_token"]) unless request.headers["auth_token"].nil?
     if @provider
       ProviderService.create_all_checked_services(@provider, provider_services_params)
       render status: :created
