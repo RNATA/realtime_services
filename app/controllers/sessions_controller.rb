edc879628d902f900a0169908a133fe14a22b39e
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-19 13:24:34 -0700

    add client true/false key to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user.authenticate(params[:password])
-      render json: { authToken: @user.auth_token }
+      render json: { authToken: @user.auth_token, client: @user.is_a? Client }
     else
       render json: {}, status: :unauthorized
     end
