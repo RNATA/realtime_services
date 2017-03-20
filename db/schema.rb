47ef057b056946bb6c567002f6e28e63c8589fcc
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-20 14:23:31 -0700

    resolve pg conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320031835) do
+ActiveRecord::Schema.define(version: 20170320185306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20170320031835) do
     t.string   "auth_token"
     t.float    "lat"
     t.float    "long"
+    t.string   "company_name"
   end
 
   create_table "services", force: :cascade do |t|
