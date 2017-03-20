3c856c7c1dd7eaa37f5ad7d7f8eaf1058d3decbe
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-20 15:03:40 -0700

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'providers/info', to: 'providers#info'
   post 'providers/activate', to: 'providers#activate'
+  post 'providers/deactivate', to: 'providers#deactivate'
   post 'providers/location', to: 'providers#location'
   post 'clients/location', to: 'clients#location'
   resources :jobs
