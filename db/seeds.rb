ce800f6557fb90b1d943f852a7b5a4c35269f218
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-20 11:09:32 -0700

    app crashed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,50 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# 10.times do
-#   Client.create!(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     email: Faker::Internet.email,
-#     street_address: Faker::Address.street_address,
-#     city: Faker::Address.city,
-#     state: Faker::Address.state,
-#     zipcode: Faker::Address.zip_code,
-#     phone_number: Faker::PhoneNumber.phone_number,
-#     password: 'password'
-#   )
-# end
+10.times do
+  Client.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    street_address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zipcode: Faker::Address.zip_code,
+    phone_number: Faker::PhoneNumber.phone_number,
+    password: 'password'
+  )
+end
 
-# SERVICE_CATEGORIES = %w{ electrical plumbing hvac miscellaneous }
+SERVICE_CATEGORIES = %w{ electrical plumbing hvac miscellaneous }
 
-# SERVICE_CATEGORIES.each do |category|
-#   Service.create!(category: category)
-# end
+SERVICE_CATEGORIES.each do |category|
+  Service.create!(category: category)
+end
 
-# 10.times do
-#   Provider.create!(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     email: Faker::Internet.email,
-#     street_address: Faker::Address.street_address,
-#     city: Faker::Address.city,
-#     state: Faker::Address.state,
-#     zipcode: Faker::Address.zip_code,
-#     phone_number: Faker::PhoneNumber.phone_number,
-#     password: 'password'
-#   )
-# end
+10.times do
+  Provider.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    street_address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zipcode: Faker::Address.zip_code,
+    phone_number: Faker::PhoneNumber.phone_number,
+    password: 'password'
+  )
+end
 
-# Provider.all.each do |provider|
-#   provider.update(services: Service.all.sample(rand(1..3)))
-# end
+Provider.all.each do |provider|
+  provider.update(services: Service.all.sample(rand(1..3)))
+end
 
-# 15.times do
-#   Job.create!(
-#     provider_service: ProviderService.find(rand(1..ProviderService.count)),
-#     client: Client.find(rand(1..Client.count))
-#   )
-# end
+15.times do
+  Job.create!(
+    provider_service: ProviderService.find(rand(1..ProviderService.count)),
+    client: Client.find(rand(1..Client.count))
+  )
+end
 
 Provider.all.each do |provider|
   provider.update(lat: rand(32.0..33.25), long: rand(-117.5..-116.85))
