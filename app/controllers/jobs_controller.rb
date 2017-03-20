87bb8638a380514b5a2b0fcf3266c425e98f6e9a
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-20 14:40:29 -0700

    job creator

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,17 @@
 class JobsController < ApplicationController
   def create
+    @client = Client.find_by(auth_token: job_params[:auth_token])
+    if @client
+      Job.create!(provider_service_id: job_params[:provider_service_id], client: @client)
+      render json: { message: ProviderService.find(job_params[:provider_service_id]).provider.first_name }
+    else
+      render status: :unauthorized
+    end
   end
 
+  private
+
   def job_params
+    params.require(:job).permit(:provider_service_id, :auth_token)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'providers/activate', to: 'providers#activate'
   post 'providers/location', to: 'providers#location'
   post 'clients/location', to: 'clients#location'
+  resources :jobs
   resources :sessions
   resources :clients
   resources :providers
